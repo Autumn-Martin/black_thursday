92cf5d620290e55796a1b0eef5f77e1fa725b2c5
Tara Craig <tara.craig7@gmail.com>
2018-08-01 11:38:14 -0600

    Rubocop transaction test and transaction repo test. Rake passing.

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -5,43 +5,34 @@ require './lib/transaction_repository'
 class TransactionRepositoryTest < Minitest::Test
   def setup
     @transactions =
-    [
-      { id: 1,
-        invoice_id: 2171,
-        credit_card_number: '4068631943231471',
-        credit_card_expiration_date: '0211',
-        result: 'success',
-        created_at: '2001-01-01 20:56:56 UTC',
-        updated_at: '2011-01-01 20:56:56 UTC'
-        },
-      {
-        id: 2,
-        invoice_id: 2172,
-        credit_card_number: '4068631943231472',
-        credit_card_expiration_date: '0212',
-        result: 'success',
-        created_at: '2002-02-02 20:56:56 UTC',
-        updated_at: '2012-02-02 20:56:56 UTC'
-        },
-      {
-        id: 3,
-        invoice_id: 2173,
-        credit_card_number: '4068631943231473',
-        credit_card_expiration_date: '0213',
-        result: 'failed',
-        created_at: '2003-03-03 20:56:56 UTC',
-        updated_at: '2013-03-03 20:56:56 UTC'
-        },
-      {
-        id: 4,
-        invoice_id: 2174,
-        credit_card_number: '4068631943231474',
-        credit_card_expiration_date: '0214',
-        result: 'success',
-        created_at: '2004-04-04 20:56:56 UTC',
-        updated_at: '2014-04-04 20:56:56 UTC'
-        }
-    ]
+      [{  id: 1,
+          invoice_id: 2171,
+          credit_card_number: '4068631943231471',
+          credit_card_expiration_date: '0211',
+          result: 'success',
+          created_at: '2001-01-01 20:56:56 UTC',
+          updated_at: '2011-01-01 20:56:56 UTC' },
+       {  id: 2,
+          invoice_id: 2172,
+          credit_card_number: '4068631943231472',
+          credit_card_expiration_date: '0212',
+          result: 'success',
+          created_at: '2002-02-02 20:56:56 UTC',
+          updated_at: '2012-02-02 20:56:56 UTC' },
+       {  id: 3,
+          invoice_id: 2173,
+          credit_card_number: '4068631943231473',
+          credit_card_expiration_date: '0213',
+          result: 'failed',
+          created_at: '2003-03-03 20:56:56 UTC',
+          updated_at: '2013-03-03 20:56:56 UTC' },
+       {  id: 4,
+          invoice_id: 2174,
+          credit_card_number: '4068631943231474',
+          credit_card_expiration_date: '0214',
+          result: 'success',
+          created_at: '2004-04-04 20:56:56 UTC',
+          updated_at: '2014-04-04 20:56:56 UTC' }]
 
     @transaction = TransactionRepository.new(@transactions)
   end
@@ -70,33 +61,33 @@ class TransactionRepositoryTest < Minitest::Test
   end
 
   def test_it_can_find_all_by_invoice_id
-    transaction_1 = @transaction.find_all_by_invoice_id(2171)
-    transaction_2 = @transaction.find_all_by_invoice_id(2172)
-    transaction_3 = @transaction.find_all_by_invoice_id(5)
-    assert_equal 2171, transaction_1.first.invoice_id
-    assert_equal 2172, transaction_2.first.invoice_id
-    assert_equal 2172, transaction_2[-1].invoice_id
-    assert_equal ([]), transaction_3
+    trans_one = @transaction.find_all_by_invoice_id(2171)
+    trans_two = @transaction.find_all_by_invoice_id(2172)
+    trans_three = @transaction.find_all_by_invoice_id(5)
+    assert_equal 2171, trans_one.first.invoice_id
+    assert_equal 2172, trans_two.first.invoice_id
+    assert_equal 2172, trans_two[-1].invoice_id
+    assert_equal [], trans_three
   end
 
   def test_it_can_find_all_by_credit_card_number
-    transaction_1 = @transaction.find_all_by_credit_card_number('4068631943231471')
-    transaction_2 = @transaction.find_all_by_credit_card_number('4068631943231472')
-    transaction_3 = @transaction.find_all_by_credit_card_number('406863194323147')
-    assert_equal '4068631943231471', transaction_1.first.credit_card_number
-    assert_equal '4068631943231472', transaction_2.first.credit_card_number
-    assert_equal '4068631943231472', transaction_2[-1].credit_card_number
-    assert_equal ([]), transaction_3
+    trans_one = @transaction.find_all_by_credit_card_number('4068631943231471')
+    trans_two = @transaction.find_all_by_credit_card_number('4068631943231472')
+    trans_three = @transaction.find_all_by_credit_card_number('406863194323147')
+    assert_equal '4068631943231471', trans_one.first.credit_card_number
+    assert_equal '4068631943231472', trans_two.first.credit_card_number
+    assert_equal '4068631943231472', trans_two[-1].credit_card_number
+    assert_equal [], trans_three
   end
 
   def test_it_can_find_all_by_result
-    transaction_1 = @transaction.find_all_by_result('success')
-    transaction_2 = @transaction.find_all_by_result('failed')
-    transaction_3 = @transaction.find_all_by_result('invalid')
-    assert_equal :success, transaction_1.first.result
-    assert_equal :failed, transaction_2.first.result
-    assert_equal :failed, transaction_2[-1].result
-    assert_equal ([]), transaction_3
+    trans_one = @transaction.find_all_by_result('success')
+    trans_two = @transaction.find_all_by_result('failed')
+    trans_three = @transaction.find_all_by_result('invalid')
+    assert_equal :success, trans_one.first.result
+    assert_equal :failed, trans_two.first.result
+    assert_equal :failed, trans_two[-1].result
+    assert_equal [], trans_three
   end
 
   def test_it_can_create_new_id
@@ -110,8 +101,7 @@ class TransactionRepositoryTest < Minitest::Test
                     credit_card_expiration_date: '0215',
                     result: 'success',
                     created_at: '2018-07-28',
-                    updated_at: '2018-07-28'
-                  }
+                    updated_at: '2018-07-28' }
     transaction = @transaction.create(attributes)
     assert_equal 5, transaction.id
     assert_equal 2775, transaction.invoice_id
@@ -129,7 +119,7 @@ class TransactionRepositoryTest < Minitest::Test
       result: 'success'
     }
     id = 1
-    transaction = @transaction.update(id, attributes)
+    @transaction.update(id, attributes)
     expected = @transaction.find_by_id(id)
     assert_instance_of Time, expected.updated_at # maybe refactor later
     assert_equal 2171, expected.invoice_id
@@ -142,7 +132,7 @@ class TransactionRepositoryTest < Minitest::Test
   def test_it_can_delete_invoice
     id = 2
 
-    invoice = @transaction.delete(id)
+    @transaction.delete(id)
     expected = @transaction.find_by_id(2)
 
     assert_nil expected
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -4,14 +4,13 @@ require './lib/transaction'
 class TransactionTest < Minitest::Test
   def setup
     @transaction_items =
-        { id: 1,
-          invoice_id: 2171,
-          credit_card_number: '4068631943231471',
-          credit_card_expiration_date: '0211',
-          result: 'success',
-          created_at: '2001-01-01 20:56:56 UTC',
-          updated_at: '2011-01-01 20:56:56 UTC'
-        }
+      { id: 1,
+        invoice_id: 2171,
+        credit_card_number: '4068631943231471',
+        credit_card_expiration_date: '0211',
+        result: 'success',
+        created_at: '2001-01-01 20:56:56 UTC',
+        updated_at: '2011-01-01 20:56:56 UTC' }
 
     @transaction = Transaction.new(@transaction_items)
   end
